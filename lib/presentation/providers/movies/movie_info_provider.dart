e8f79c409e4d2a43fb191d10ddfcd3bca69a0cd6
Esosanin <miguel0307jm@gmail.com>
2026-04-08 18:17:52 -0700

    movie details check

---
+++ lib/presentation/providers/movies/movie_info_provider.dart
@@ -0,0 +1,38 @@
+import 'package:cinemapedia/presentation/providers/providers.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:flutter_riverpod/legacy.dart';
+
+
+final movieInfoProvider = StateNotifierProvider((ref) {
+  final movieRepository = ref.watch( movieRepositoryProvider );
+  return MovieMapNotifier( getMovie: movieRepository.getMovieById );
+});
+
+// ignore: slash_for_doc_comments
+/**
+ * {
+ *  '505642': Movie(),
+ *  '505643': Movie(),
+ *  '505644': Movie(),
+ *  '505645': Movie(),
+ * }
+ */
+
+typedef GetMovieCallback = Future<Movie>Function(String movieId);
+
+class MovieMapNotifier extends StateNotifier<Map<String, Movie>> {
+
+  final GetMovieCallback getMovie;
+
+  MovieMapNotifier({
+    required this.getMovie,
+  }): super({});
+
+  Future<void> loadMovie( String movieId ) async {
+    if( state[movieId] != null ) return;
+
+    final movie = await getMovie( movieId );
+
+    state = { ...state, movieId: movie };
+  }
+}
